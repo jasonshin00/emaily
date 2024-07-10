efd07184e5306cfe40ee4fcb00a5661ebf2643d9
Jason Shin <jasonshin@Jasons-MacBook-Pro.local>
2024-07-10 15:58:17 -0700

    finished auth flow

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({'hi' : 'there'});
-});
+//middleware
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.listen(4000);
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 4000; //render address or 4000
+app.listen(PORT);
